19783933b52ca3e8c945efca84e3b2bd2e114590
leovasc5 <leovasc5@hotmail.com>
2021-06-29 09:49:04 -0300

    Building

--- exercicio1/jogoDaVelha.py
+++ exercicio1/jogoDaVelha.py
@@ -4,13 +4,13 @@ from colorama import Fore, Back, Style
 
 jogarNovamente = ""
 jogadas = 0
-quemJoga = 2
+quemJoga = 2 #1 = CPU | #2 = Jogador
 maxJogadas = 9
 vitoria = ""
 matriz = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]]
 
 def tela():
-    global velha
+    global matriz
     global jogadas
     os.system("cls")
     print("     1    2    3")
@@ -21,8 +21,50 @@ def tela():
     print("3:    " + matriz[2][0] + " | " + matriz[2][1] + " | "+ matriz[2][2])
     print("Jogadas: " + Fore.GREEN + str(jogadas) + Fore.RESET)
 
-#while True:
+def jogadorJoga():
+    global jogadas
+    global quemJoga
+    global maxJogadas
+
+    if quemJoga == 2 and jogadas < maxJogadas:
+        l = int(input("Linha..: "))
+        l = l-1
+
+        c = int(input("Coluna.: "))
+        c = c-1
+        
+        try:    
+            while matriz[l][c] != " ":
+                l = int(input("Linha..: "))
+                l = l-1
+
+                c = int(input("Coluna.: "))
+                c = c-1
+
+            matriz[l][c] = "X"
+            quemJoga = 1
+            jogadas+=1
+        except:
+            print("Espaço inválido!")
+
+def cpuJoga():
+    global jogadas
+    global maxJogadas
+    global quemJoga
+
+    if quemJoga == 1 and jogadas < maxJogadas:
+        l = random.randrange(0, 3)
+        c = random.randrange(0, 3)
+
+        while matriz [l][c] != " ":
+            l = random.randrange(0, 3)
+            c = random.randrange(0, 3)
+        
+        matriz[l][c] = "O"
+        quemJoga = 2
+        jogadas+=1
+        
+while True:
     tela()
-
-
-tela()
+    jogadorJoga()
+    cpuJoga()
