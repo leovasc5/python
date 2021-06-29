42e26bba7c258419c0846f3f8309b59e3c461baf
Leonardo Vasconcelos Paulino <leovasc5@hotmail.com>
2021-06-29 08:17:40 -0300

    Aula 28

---
+++ aula28/aula28.py
@@ -0,0 +1,8 @@
+jogadores = ["Cristiano Ronaldo", "Messi", "Neymar", "De Bruyne"]
+
+itJogadores = iter(jogadores)
+print(next(itJogadores))
+print(next(itJogadores))
+
+# for jogador in jogadores:
+#     print(jogador)
