df1e0e064d2aab1b62e26049d2d4cc0ee4351dfe
leovasc5 <leovasc5@hotmail.com>
2021-07-01 15:46:15 -0300

    Aula 55

---
+++ aula55/aula55.py
@@ -0,0 +1,12 @@
+file = open("C:/Users/l5/python/aula55/teste.txt", "a")
+
+file.write("\n" + input("Digite o texto: "))
+
+file.close()
+
+# r = read
+# a = append
+# w = write
+# x = create
+# t = texto
+# b = binary
