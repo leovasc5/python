8b9e0e12d9f0bdc5a8eb9d0191723e10742f3fa6
leovasc5 <leovasc5@hotmail.com>
2021-07-01 14:27:00 -0300

    Aula 50

---
+++ aula50/aula50.py
@@ -0,0 +1,15 @@
+import re
+texto = "Olá_mundo!_Eu_sou_Leonardo_Vasconcelos"
+
+while True:
+    search = input("Pesquisar: ")
+    res = re.findall(search.lower(), texto.lower())
+    print("\nResultados:")
+
+    if len(res) == 0:
+        print("Não foi encontrado nada\n")
+    else:
+        for i in res:
+            print(i+"\n")
+    
+        
