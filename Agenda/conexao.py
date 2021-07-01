f98ee421703a4834276fb098d29d73add2b5fa0d
leovasc5 <leovasc5@hotmail.com>
2021-07-01 20:15:39 -0300

    Criada a função para inserir contatos

--- Agenda/conexao.py
+++ Agenda/conexao.py
@@ -2,10 +2,28 @@ import sqlite3
 from sqlite3 import Error
 
 def ConexaoBanco():
-    caminho = "C:\\Users\\l5\\python\\Agenda\\banco\\banco.db"
+    caminho = "C:\\Users\\l5\\python\\Agenda\\banco\\agenda.db"
     con = None
     try: 
         con = sqlite3.connect(caminho)
     except Error:
         print(Error)
     return con
+
+nome = input("Digite o nome: ")
+telefone = input("Digite o telefone: ")
+email = input("Digite o email: ")
+
+sql = """INSERT INTO tb_contatos (T_NOMECONTATO, T_TELEFONECONTATO, T_EMAILCONTATO)
+        VALUES ('"""+nome+"""','"""+telefone+"""','"""+email+"""')"""
+
+def inserir(conexao, sql):
+    try:
+        c = conexao.cursor()
+        c.execute(sql)
+        conexao.commit()
+        print("Registro inserido!")
+    except Error:
+        print(Error)
+con = ConexaoBanco()
+inserir(con, sql)
