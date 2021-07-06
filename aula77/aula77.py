8d9f8d0e62b8601737964d6f35c1cb8f57ec3681
leovasc5 <leovasc5@hotmail.com>
2021-07-06 15:06:51 -0300

    Aula 77

---
+++ aula77/aula77.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+app = Tk()
+app.title("Aula 76")
+app.geometry("300x300")
+
+def showValor():
+    print("Valor: "+str(sb_valores1.get()))
+
+sb_valores1 = Spinbox(app, from_=1, to=10)
+sb_valores1.pack()
+
+btn1 = Button(app, text="Defina um valor", command=showValor)
+btn1.pack()
+
+sb_valores2 = Spinbox(app, values=(1,2,3,4,5))
+sb_valores2.pack()
+
+app.mainloop()
