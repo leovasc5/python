8c2718c8d61f6849161bb2150a97db63ba427786
leovasc5 <leovasc5@hotmail.com>
2021-07-06 15:36:36 -0300

    Aula 78

--- aula78/aula78.py
+++ aula78/aula78.py
@@ -5,3 +5,19 @@ app = Tk()
 app.title("Aula 76")
 app.geometry("300x300")
 
+nb = ttk.Notebook(app)
+nb.place(x=0 ,y=0, width=300, height=300)
+
+tb1 = Frame(nb)
+tb2 = Frame(nb)
+
+nb.add(tb1, text="Primeiro")
+nb.add(tb2, text="Segundo")
+
+lb1 = Label(tb1, text="Sou a primeira parte!")
+lb1.pack()
+
+lb2 = Label(tb2, text="Sou a segunda parte!")
+lb2.pack()
+
+app.mainloop()
