3087bd37239190a7243d30ba8b641d08e6f7b6c3
leovasc5 <leovasc5@hotmail.com>
2021-07-11 23:03:46 -0300

    Aula 85

---
+++ aula85/aula85.py
@@ -0,0 +1,28 @@
+from tkinter import *
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+import os
+
+def convert(mm):
+    return mm/0.352777    
+    
+def criarPDF():
+    try:
+        cnv = canvas.Canvas(os.path.dirname(__file__)+"\\exemplo.pdf", pagesize=A4)
+        cnv.setTitle("Arquivo de Teste")
+        cnv.drawImage(os.path.dirname(__file__)+"\\python.png", convert(15), convert(110), mask="auto")
+        cnv.drawString(convert(80), convert(90), "Leonardo Vasconcelos")
+        cnv.grid(["a", "b", "c"],["1", "2", "3"], convert(80), convert(40))
+        cnv.save()
+        print("PDF Criado")
+    except:
+        print("Erro")
+
+app = Tk()
+app.title("Aula 85")
+app.geometry("320x350")
+
+btn1 = Button(app, text="Criar PDF", command=criarPDF)
+btn1.place(x=60, y=60)
+
+app.mainloop()
