ce1df76920265bd01f4af4e141e41fa8361ac648
leovasc5 <leovasc5@hotmail.com>
2021-07-14 14:32:49 -0300

    Aula 95

---
+++ aula95/aula95.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+dados = pd.read_csv("C:/Users/l5/python/aula38/planilha2.csv", encoding="UTF-8", sep=",", header=0, 
+usecols=["league","season","position","team","matches","wins","draws","loses","scored","missed","pts"], nrows=120)
+dados.columns = ['liga','temporada','posicao','time','partidas','vitorias','empates','derrotas','gols pro','gols contra','pontos']
+
+teste = pd.Series(np.random.randn(5), name="teste")
+print(teste.head())
