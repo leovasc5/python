2c83672763552a0cd21866d92e2f2fa856725a7e
leovasc5 <leovasc5@hotmail.com>
2021-07-15 13:48:02 -0300

    Aula 102

---
+++ aula102/aula102.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+dados = pd.read_csv("C:/Users/l5/python/aula38/planilha2.csv", encoding="UTF-8", sep=",", header=0, 
+usecols=["league","season","position","team","matches","wins","draws","loses","scored","missed","pts"], nrows=120)
+
+dados.columns = [x.capitalize() for x in dados.columns.to_list()]
+print(dados.head())
