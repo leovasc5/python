2c3291758a7fdba72c17f9e8003f9f695f38eaed
leovasc5 <leovasc5@hotmail.com>
2021-07-16 18:15:58 -0300

    Aula 115

--- aula115/aula115.py
+++ aula115/aula115.py
@@ -12,6 +12,16 @@ gastos = pd.Series([540.89, 1025.07, 124.00, "-", 1025.67, 87.00])
 dados = zip(lucros, gastos)
 df2 = pd.DataFrame(data=dados, index=pd.date_range("07/01/2021", periods=6), columns=["Lucro", "Gasto"])
 
+lucros = pd.Series(["R$ 1.024,50", "R$ 500,06", "R$ 1.540,00", "R$ 2.789,80", "R$ 1.000,32", "R$ 1.821,50"])
+gastos = pd.Series(["R$ 540,89", "R$ 1.025,07", "R$ 124,00", "R$ 1.084", "R$ 1.025,67", "R$ 87,00"])
+lucros = lucros.apply(lambda x: x.replace("R$ ", "").replace(".", "").replace(",", "."))
+gastos = gastos.apply(lambda x: x.replace("R$ ", "").replace(".", "").replace(",", "."))
+dados = zip(lucros, gastos)
+df3 = pd.DataFrame(data=dados, index=pd.date_range("07/01/2021", periods=6), columns=["lucros", "gastos"])
+df3["lucros"] = df3["lucros"].apply(float)
+df3["gastos"] = df3["gastos"].apply(float)
+df3["saldo"] = (df3["lucros"] - df3["gastos"])
+
 novaSeriesLucro = []
 for i in df["Lucro"]:
     if np.isnan(i):
@@ -31,5 +41,5 @@ df["Gasto"] = novaSeriesGasto
 df2["Lucro"] = df2["Lucro"].replace("-", 0)
 df2["Gasto"] = df2["Gasto"].replace("-", 0)
 
-# print(df)
-print(df2)
+print(df3.filter(["saldo"]))
+df3 = df3.drop(["saldo"], axis=1)
