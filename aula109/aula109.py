3660b96235bb1a9b2bba975ddb6ff609453ee905
leovasc5 <leovasc5@hotmail.com>
2021-07-16 00:02:35 -0300

    Aula 109

---
+++ aula109/aula109.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+dados = pd.read_csv("C:/Users/l5/python/aula38/planilha2.csv", encoding="UTF-8", sep=",", header=0, 
+usecols=["league","season","position","team","matches","wins","draws","loses","scored","missed","pts"], nrows=120)
+dados.columns = ['liga','temporada','posicao','time','partidas','vitorias','empates','derrotas','gols pro','gols contra','pontos']
+
+print(dados.time.value_counts()) #Conta as repetições
+print("\n")
+print(dados.sample(n=10, random_state=5248544))
